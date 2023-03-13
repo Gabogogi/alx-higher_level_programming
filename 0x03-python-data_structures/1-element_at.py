d70eb53d7545b53f69c735c59fa8069d1540ee2f
Gabogogi <bencitymaina@gmail.com>
2023-03-12 21:50:17 -0700

    f

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+
 def element_at(my_list, idx):
-    if int(idx) < 0 or int(idx) > len(my_list):
+    if idx < 0 or idx >= len(my_list):
         return None
 
-    return(my_list[int(idx)])
+    return(my_list[idx])
