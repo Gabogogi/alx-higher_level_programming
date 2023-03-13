7fe705b1842f91e63c7df68bdaa7ce3da5cd225d
Gabogogi <bencitymaina@gmail.com>
2023-03-12 21:47:41 -0700

    g

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,4 +3,4 @@ def element_at(my_list, idx):
     if int(idx) < 0 or int(idx) > len(my_list):
         return None
 
-    return print("Element at index {:d} is {}".format(int(idx), my_list[int(idx)]))
+    return(my_list[int(idx)])
