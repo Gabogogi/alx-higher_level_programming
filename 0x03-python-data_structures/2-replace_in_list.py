cb516d76a111cde219bedc7f8094dadca45fe188
Gabogogi <bencitymaina@gmail.com>
2023-03-12 22:01:00 -0700

    f

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
-def replace_in_list(my_list, idx, new_element):
+def replace_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
         return my_list
     
-    my_list[idx] = new_element
+    my_list[idx] = element
     
     return my_list
