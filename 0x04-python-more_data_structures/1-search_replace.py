6733e5c4b288f0148915bc7cfe7d0426c1301988
Gabogogi <bencitymaina@gmail.com>
2023-03-15 23:26:59 +0300

    g

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = []
-    for i in range(len(my_list)):
-        if my_list[i] == search:
-            my_list[i] = replace
-        new_list.append(my_list[i])
-    return new_list
+    new_list = list(map(lambda x: replace if x == search else x, my_list))
+    return (new_list)
--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    new_dir = a_dictionary.copy()
+    list_keys = list(new_dir.keys())
 
-def multiply_values_by_2(d):
-    new_d = {}
-    for key, value in d.items():
-        new_d[key] = value * 2
-    return new_d
+    for i in list_keys:
+        new_dir[i] *= 2
+
+    return (new_dir)
