f2888bd2713b9c92e389aed3df5d7ecece4981b7
Gabogogi <bencitymaina@gmail.com>
2023-03-15 23:33:24 +0300

    g

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,9 +2,11 @@
 
 def best_score(a_dictionary):
     max_val = 0
-    for x in a_dictionary.values():
+    if x in a_dictionary.values():
         if max_val < x:
             max_val = x
+    else:
+        return None
     key_list = list(newdict.keys())
     val_list = list(newdict.values())
     pos = val_list.index(max_val)
