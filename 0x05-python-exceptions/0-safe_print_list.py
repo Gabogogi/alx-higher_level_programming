9810c418bacf04bcff9e3854df14556ac0c5fbee
Gabogogi <bencitymaina@gmail.com>
2023-03-27 16:46:29 +0300

    g

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        i = 0
-        while i < x:
-            print(my_list[i], end="")
+    i = 0
+    for m in range(x):
+        try:
+            print(my_list[m], end='')
             i += 1
-    except IndexError:
-        pass
+        except IndexError:
+            break
+    print()
+    return i
