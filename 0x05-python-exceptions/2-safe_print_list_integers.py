a7da789b5dc9b6de545ecfa28c13c7efc967c379
Gabogogi <bencitymaina@gmail.com>
2023-03-28 00:05:10 +0300

    g

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,8 +4,9 @@ def safe_print_list_integers(my_list=[], x=0):
     for m in range(x):
         try:
             print("{:d}".format(my_list[m]), end='')
-            i += 1
         except (ValueError, TypeError):
             continue
+        else:
+            i += 1
     print()
     return i
