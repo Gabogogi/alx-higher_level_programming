d79893ff82e6ffa4da8c803a2749d2cb0013b9e2
Gabogogi <bencitymaina@gmail.com>
2023-03-28 19:29:02 +0300

    g

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-""" defines a square """
-
-
 class Square:
     """ square with private instance attribute size """
 
@@ -18,3 +15,6 @@ class Square:
                 self.__size = size
         else:
             raise TypeError('size must be an integer')
+        
+    def area(self, size):
+        return size * size
