5141fc04f0014e5791b1bc62682bbfe5abd41042
Gabogogi <bencitymaina@gmail.com>
2023-09-04 08:24:18 -0700

    made integer

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -99,4 +99,4 @@ class Rectangle:
 
     def __repr__(self):
         """ return a string representation of the rectangle """
-        return "Rectangle({}, {})".format(self.__width, self.__height)i
+        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
