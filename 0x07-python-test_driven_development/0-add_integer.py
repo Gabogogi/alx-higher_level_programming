6a0375643dd5b5bc9bbadd02016c8d7470447610
Gabogogi <bencitymaina@gmail.com>
2023-09-05 10:11:58 +0300

    f

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,10 @@
+def add_integer(a, b=98):
+    """
+    Adds two integer. casting them to integers if needed"""
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer".format(a))
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    a = int(a)
+    b = int(b)
+    return "{:d}".format(a + b)
---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,20 @@
+def matrix_divided(matrix, div):
+    result = [[0 for _ in range(len(matrix[0]))] for _ in range(len(matrix))]
+    for outer_list in matrix:
+        for item in outer_list:
+            if not isinstance(item, (int, float)):
+                raise TypeError("matrix must be a matrix of integers/floats")
+    first_row = len(matrix[0])
+    for row in matrix:
+        if len(row) != first_row:
+            raise TypeError("Each row of the matrix must have the same size")
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            result[i][j] = "{:.2f}".format(matrix[i][j] / div)
+
+    return result
+
