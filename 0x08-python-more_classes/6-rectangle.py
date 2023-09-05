70381e0cf76e268e27e6d09b2a0163a2644aa3f8
Gabogogi <bencitymaina@gmail.com>
2023-09-05 12:19:35 -0700

    d

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -24,6 +24,7 @@ class Rectangle:
     def __del__(self):
         """delete class instance"""
         print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
 
     @property
     def width(self):
