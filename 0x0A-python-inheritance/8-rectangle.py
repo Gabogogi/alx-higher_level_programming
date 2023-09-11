e7e88dd8cacd62c3addad29eafc5650489914987
Gabogogi <bencitymaina@gmail.com>
2023-09-11 07:33:21 -0700

    f

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""a class based on 7-base geometry"""
+
+
+class BaseGeometry:
+    """a class defining a shape"""
+
+    def area(self):
+        """raises exception since area is not defined"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """function that validates value"""
+        if not isinstance(value, int):
+            raise TypeError("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """defines a rectangle from geometry class"""
+    def __init__(self, width, height):
+        """initializes a new rectangle"""
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
