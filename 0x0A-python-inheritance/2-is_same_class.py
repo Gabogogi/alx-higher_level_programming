20dbbe23ca64c17b64e7f1123a2392b020275e94
Gabogogi <bencitymaina@gmail.com>
2023-09-11 05:57:01 -0700

    234

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -2,6 +2,7 @@
 
 """Checks exact class"""
 
+
 def is_same_class(obj, a_class):
     """ function that returns True if the object is
     exactly an instance of the specified class ; otherwise False"""
--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -2,6 +2,7 @@
 
 """Checks for direct or indirect inheritance"""
 
+
 def inherits_from(obj, a_class):
     """returns True if the object is an instance of a
     class that inherited (directly or indirectly)
