64a922ea267a4194e7d3a0c740467859e5fdd5a1
Gabogogi <bencitymaina@gmail.com>
2023-09-11 12:13:12 -0700

    f

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''
+Contains a class MyList
+'''
+
+class MyList(list):
+    '''
+    A class that inherits from list
+    '''
+    def __init__(self):
+        '''initializes the object'''
+        super().__init__()
+
+    def print_sorted(self):
+        '''that prints the list, but sorted'''
+        print(sorted(self))
