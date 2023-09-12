ffc964290b2f24738110bab6c835842acdabb5ac
Gabogogi <bencitymaina@gmail.com>
2023-09-11 23:41:23 -0700

    0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''
+Contains a a function that reads a text file
+'''
+
+
+def read_file(filename=""):
+    '''Reads a text file'''
+    with open(filename, encoding="utf-8") as f:
+        print(f.read())
