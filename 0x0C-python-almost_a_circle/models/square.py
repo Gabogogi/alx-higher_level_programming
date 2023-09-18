23e6db3026551bd32c57255eea31858035329b98
Gabogogi <bencitymaina@gmail.com>
2023-09-18 04:49:38 -0700

    f

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -34,23 +34,21 @@ class Square(Rectangle):
 
     def update(self, *args, **kwargs):
         """Update the Square."""
-        def update(self, *args, **kwargs):
-    '''Update attributes based on positional and keyword arguments'''
-    if args:
+        if args:
         # If positional arguments are provided, update attributes in order
-        if len(args) >= 1:
-            self.id = args[0]
-        if len(args) >= 2:
-            self.size = args[1]
-        if len(args) >= 3:
-            self.x = args[2]
-        if len(args) >= 4:
-            self.y = args[3]
-    else:
-        # If no positional arguments, update attributes using keyword arguments
-        for key, value in kwargs.items():
-            setattr(self, key, value)      
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+        else:
+            # If no positional arguments, update attributes using keyword arguments
+            for key, value in kwargs.items():
+                setattr(self, key, value)      
 
     def __str__(self):
         """Return the print() and str() representation of a Square."""
-        return "[Square] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y, self.width, self.width)
+        return "[Square] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y, self.width)
