2d99d575dc5ab1a0d1565754b76b105dd9317a5e
Gabogogi <bencitymaina@gmail.com>
2023-09-18 14:06:47 +0300

    ff

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -23,7 +23,6 @@ class Base:
         '''returns JSON string representation of dict'''
         if not list_dictionaries:
             return "[]"
-
         return json.dumps(list_dictionaries)
 
     @classmethod
@@ -38,13 +37,28 @@ class Base:
         json_string = cls.to_json_string(list_of_dicts)
         with open(filename, "w") as file:
             file.write(json_string)
-
+    
+    @staticmethod
     def from_json_string(json_string):
         '''returns the list of the JSON string representation'''
         if json_string is None:
             json_string = []
         return json.loads(json_string)
 
+    @classmethod
+    def create(cls, **dictionary):
+        """Return a class instantied from a dictionary of attributes.
+        Args:
+            **dictionary (dict): Key/value pairs of attributes to initialize.
+        """
+        if dictionary and dictionary != {}:
+            if cls.__name__ == "Rectangle":
+                new = cls(1, 1)
+            else:
+                new = cls(1)
+            new.update(**dictionary)
+            return new
+    
     @classmethod
     def load_from_file(cls):
         """Return a list of classes instantiated from a file of JSON strings"""
@@ -56,6 +70,22 @@ class Base:
                 instances = [cls.create(**d) for d in list_of_dicts]
                 return instances
         except FileNotFoundError:
+            return []
+
+    @classmethod
+    def load_from_file(cls):
+        """Return a list of classes instantiated from a file of JSON strings.
+        Reads from `<cls.__name__>.json`.
+        Returns:
+            If the file does not exist - an empty list.
+            Otherwise - a list of instantiated classes.
+        """
+        filename = str(cls.__name__) + ".json"
+        try:
+            with open(filename, "r") as jsonfile:
+                list_dicts = Base.from_json_string(jsonfile.read())
+                return [cls.create(**d) for d in list_dicts]
+        except IOError:
             return []
 
     @classmethod
@@ -93,3 +123,43 @@ class Base:
                     instances.append(instance)
         except FileNotFoundError:
             pass
+
+    @staticmethod
+    def draw(list_rectangles, list_squares):
+        """Draw Rectangles and Squares using the turtle module.
+        Args:
+            list_rectangles (list): A list of Rectangle objects to draw.
+            list_squares (list): A list of Square objects to draw.
+        """
+        turt = turtle.Turtle()
+        turt.screen.bgcolor("#b7312c")
+        turt.pensize(3)
+        turt.shape("turtle")
+
+        turt.color("#ffffff")
+        for rect in list_rectangles:
+            turt.showturtle()
+            turt.up()
+            turt.goto(rect.x, rect.y)
+            turt.down()
+            for i in range(2):
+                turt.forward(rect.width)
+                turt.left(90)
+                turt.forward(rect.height)
+                turt.left(90)
+            turt.hideturtle()
+
+        turt.color("#b5e3d8")
+        for sq in list_squares:
+            turt.showturtle()
+            turt.up()
+            turt.goto(sq.x, sq.y)
+            turt.down()
+            for i in range(2):
+                turt.forward(sq.width)
+                turt.left(90)
+                turt.forward(sq.height)
+                turt.left(90)
+            turt.hideturtle()
+
+        turtle.exitonclick()
