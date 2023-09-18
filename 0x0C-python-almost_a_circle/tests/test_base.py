3884fe36c7f60e00c522b479d790175557771876
Gabogogi <bencitymaina@gmail.com>
2023-09-18 10:40:07 +0300

    d

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+'''tests for base.py'''
+
+
+import os
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
+
+
+class TestBase_instantiation(unittest.TestCase):
+    '''unit test for instantiation'''
+
+    def test_no_args_id(self):
+        b1 = Base()
+        b2 = Base()
+        self.assertEqual(b1.id, b2.id - 1)
