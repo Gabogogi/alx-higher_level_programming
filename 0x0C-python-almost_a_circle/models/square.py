42248cdf802179eb40598eba62bf5fdba6a3e567
Gabogogi <117763241+Gabogogi@users.noreply.github.com>
2023-09-18 14:32:26 +0300

    Update square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -34,35 +34,22 @@ class Square(Rectangle):
 
     def update(self, *args, **kwargs):
         """Update the Square."""
-        if args and len(args) != 0:
-            a = 0
-            for arg in args:
-                if a == 0:
-                    if arg is None:
-                        self.__init__(self.size, self.x, self.y)
-                    else:
-                        self.id = arg
-                elif a == 1:
-                    self.size = arg
-                elif a == 2:
-                    self.x = arg
-                elif a == 3:
-                    self.y = arg
-                a += 1
-
-        elif kwargs and len(kwargs) != 0:
-            for k, v in kwargs.items():
-                if k == "id":
-                    if v is None:
-                        self.__init__(self.size, self.x, self.y)
-                    else:
-                        self.id = v
-                elif k == "size":
-                    self.size = v
-                elif k == "x":
-                    self.x = v
-                elif k == "y":
-                    self.y = v
+        def update(self, *args, **kwargs):
+    '''Update attributes based on positional and keyword arguments'''
+    if args:
+        # If positional arguments are provided, update attributes in order
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.size = args[1]
+        if len(args) >= 3:
+            self.x = args[2]
+        if len(args) >= 4:
+            self.y = args[3]
+    else:
+        # If no positional arguments, update attributes using keyword arguments
+        for key, value in kwargs.items():
+            setattr(self, key, value)      
 
     def __str__(self):
         """Return the print() and str() representation of a Square."""
