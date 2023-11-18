0b4b93330e6b8ea74137fe01bb04548ac5334b91
Gabogogi <bencitymaina@gmail.com>
2023-11-18 19:20:26 +0300

    task 2

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''
+Gets all states in hbtn_0e_0_usa database
+all states with a name starting with N (upper N)
+'''
+import MySQLdb
+import sys
+
+
+if __name__ =="__main__":
+    if len(sys.argv) != 4:
+        print("Usage: <username> <password> <databaseName>")
+        exit(1)
+    username = sys.argv[1]
+    password = sys.argv[2]
+    name = sys.argv[3]
+
+    connection = MySQLdb.connect(user=username, password=password, database=name)
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
+    result = cursor.fetchall()
+    for i in result:
+        print(i)
+    cursor.close()
+   
+
