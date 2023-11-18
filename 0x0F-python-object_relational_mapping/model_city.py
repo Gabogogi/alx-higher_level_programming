617ecdea870862449050a62bb97a53dd05e705dd
Gabogogi <bencitymaina@gmail.com>
2023-11-19 00:22:34 +0300

    correct 0

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -9,13 +9,14 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
+
 Base = declarative_base()
 
 class City(Base):
     '''
     Script to list all cities of a specified state from a MySQL database.
     '''
-    __tablename__ = 'cites'
+    __tablename__ = 'cities'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey("states.id"), nullable=False, )
