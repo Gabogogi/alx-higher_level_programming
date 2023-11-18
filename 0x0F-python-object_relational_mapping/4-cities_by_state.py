0d78f88ecb826359ee7bf8426a1a38026de02f45
Gabogogi <bencitymaina@gmail.com>
2023-11-18 20:57:32 +0300

    task 4

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''
+Gets all states in hbtn_0e_0_usa dtabase
+'''
+import MySQLdb
+import sys
+
+
+if __name__ =="__main__":
+    if len(sys.argv) != 4:
+        print("Usage: <username> <password> <databaseName>")
+        exit(1)
+    username = sys.argv[1]
+    password = sys.argv[2]
+    name = sys.argv[3]
+
+    connection = MySQLdb.connect(user=username, password=password, database=name)
+    cursor = connection.cursor()
+    cursor.execute("SELECT cities.id, cities.name, states.name FROM cities, states WHERE cities.state_id = states.id;")
+    result = cursor.fetchall()
+    for i in result:
+        print(i)
+    cursor.close()
+   
+
