3fb1cdb18531cffe3592215caef6460ca2a0388f
Gabogogi <bencitymaina@gmail.com>
2023-11-18 20:22:33 +0300

    correct 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,7 +7,7 @@ import sys
 
 
 if __name__ =="__main__":
-    if len(sys.argv) != 4:
+    if len(sys.argv) != 5:
         print("Usage: <username> <password> <databaseName>")
         exit(1)
     username = sys.argv[1]
@@ -18,16 +18,19 @@ if __name__ =="__main__":
     
 
 
-    connection = MySQLdb.connect(user=username, password=password, database=db_name)
-    cursor = connection.cursor()
+    db = MySQLdb.connect(user=username, password=password, database=db_name)
+
     
-    state_name = "SELECT * FROM states LIKE BINARY '{}'".format(name)
-    cursor.execute(state_name)
-    result = cursor.fetchall()
-    for result in result:
-        if result[1] == name:
-            print(result)
-    cursor.close()
+    cur = db.cursor()
+    nmeSr = "SELECT * FROM states WHERE name LIKE BINARY '{}'".format(sys.argv[4])
+    cur.execute(nmeSr)
+
+    rows = cur.fetchall()
+    for i in rows:
+        print(i)
+ 
+    cur.close()
+    db.close()
 
    
 
