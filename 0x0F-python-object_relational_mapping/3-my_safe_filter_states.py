b4cf3929c22f876bb27d90c0a28de61d85fd7f86
Gabogogi <bencitymaina@gmail.com>
2023-11-18 20:27:29 +0300

    task 2

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,1 +1,36 @@
-hey
+#!/usr/bin/python3
+'''
+takes in an argument and displays all values in the states
+'''
+import MySQLdb
+import sys
+
+
+if __name__ =="__main__":
+    if len(sys.argv) != 5:
+        print("Usage: <username> <password> <databaseName>")
+        exit(1)
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    name = sys.argv[4]
+
+    
+
+
+    db = MySQLdb.connect(user=username, password=password, database=db_name)
+
+    
+    cur = db.cursor()
+    nmeSr = "SELECT * FROM states WHERE name LIKE BINARY %s"
+    cur.execute(nmeSr, (name,))
+
+    rows = cur.fetchall()
+    for i in rows:
+        print(i)
+ 
+    cur.close()
+    db.close()
+
+   
+
