5099a12980ec30db20a023874c9ca0827d0b6331
Gabogogi <bencitymaina@gmail.com>
2023-11-19 11:20:48 +0300

    correction to model states

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -2,12 +2,9 @@
 '''
 Improve the files  model_state.py
 '''
-from sqlalchemy import (create_engine, ForeignKey,
-                        String, Integer, Column, CHAR)
-
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, relationship
-
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
@@ -18,4 +15,4 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", back_populates="state", cascade="all, delete-orphan")
+    cities = relationship("City", backref="state", cascade="all, delete")
